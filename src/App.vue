c00f4a843f52a95f00ea6fd13b9bf7df1497e501
xinxin5080 <xinxin5080@163.com>
2018-12-23 19:04:26 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <increment></increment>
+    <decrement></decrement>
+  </div>
+</template>
+<script>
+import Increment from './views/Increment.vue'
+import Decrement from './views/Decrement.vue'
+export default {
+  name: 'App',
+  components: {
+    Increment,
+    Decrement
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Decrement.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    当前值为：<el-tag type="danger">xxx</el-tag><el-button>-1</el-button>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
